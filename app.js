98eafdf6728b9aad95a1241bff937deea4368002
pydevsg <ghoshsudipto1129@gmail.com>
2020-04-29 17:43:51 +0530

    changed naming conventions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let express = require('express')
-let app = express()
-let bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var restraulist = [
+var restaurantlist = [
 	{"id": 1,"name": "La Casino","place": "Arizona","cuisine_type": "Itailian",rating:4.5},
 	{"id": 2,"name": "Roberta's Pizza","place": "Brooklyn","cuisine_type": "American",rating:4.3},
 	{"id": 3,"name": "Hometown BBQ","place": "New York","cuisine_type": "American",rating:4.2},
@@ -9,28 +9,28 @@ var restraulist = [
 ];
 
 router.get('/',function(req,res){
-	res.json(restraulist);
+	res.json(restaurantlist);
 });
-router.get('/api/restraulist/:id', function(req, res){
-   const restra = restraulist.find(c=>c.id === parseInt(req.params.id));
+router.get('/api/restaurant/:id', function(req, res){
+   const restra = restaurantlist.find(c=>c.id === parseInt(req.params.id));
    if(!restra) res.status(404).send('Restaurant data not found');
    res.send(restra);
 });
 
-router.post('/api/restraulist/',function(req,res){
+router.post('/api/restaurant/',function(req,res){
 	const newrest = {
-		id:restraulist.length+1,
+		id:restaurantlist.length+1,
 		name:req.body.name,
 		place:req.body.name,
 		cuisine_type:req.body.name,
 		rating:req.body.rating
 	};
-	restraulist.push(newrest);
+	restaurantlist.push(newrest);
 	res.send(newrest);
 });
 
-router.put('/api/restraulist/:id',function(req,res){
-	const restra = restraulist.find(c=>c.id === parseInt(req.params.id));
+router.put('/api/restaurant/:id',function(req,res){
+	const restra = restaurantlist.find(c=>c.id === parseInt(req.params.id));
 	if(!restra){
 		return res.status(400).send('Restaurant data not found');
 	}
@@ -40,13 +40,13 @@ router.put('/api/restraulist/:id',function(req,res){
 	res.send(restra);
 });
 
-router.delete('/api/restraulist/:id',function(req,res){
-	const delrest = restraulist.find(c=>c.id === parseInt(req.params.id));
+router.delete('/api/restaurant/:id',function(req,res){
+	const delrest = restaurantlist.find(c=>c.id === parseInt(req.params.id));
 	if(!delrest){
 		return res.status(400).send('Restaurant data not found');
 	}
-	const index = restraulist.indexOf(delrest);
-	restraulist.splice(index,1);
+	const index = restaurantlist.indexOf(delrest);
+	restaurantlist.splice(index,1);
 	res.send(delrest);
 })
 module.exports = router;
